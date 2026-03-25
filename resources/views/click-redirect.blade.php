327df68c5a5bc0af148753ea1ebd7555d17372fc
ThanhLuan1903 <thanhluan1903@gmail.com>
2026-03-25 09:05:25 +0700

    fix: update the referer to get the data url

--- resources/views/click-redirect.blade.php
+++ resources/views/click-redirect.blade.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="referrer" content="unsafe-url">
-    <meta http-equiv="refresh" content="0;url={{ $targetUrl }}">
     <title>Redirecting...</title>
-    <script>
-        window.location.replace(@json($targetUrl));
-    </script>
 </head>
 <body>
-    <p>Redirecting...</p>
-    <p>
-        <a href="{{ $targetUrl }}" rel="noreferrer">Continue</a>
-    </p>
+<script>
+    const cleanUrl = @json($currentClickUrl);
+    const targetUrl = @json($targetUrl);
+
+    try {
+        history.replaceState({}, '', cleanUrl);
+    } catch (e) {}
+
+    window.location.replace(targetUrl);
+</script>
 </body>
 </html>
